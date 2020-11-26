46855611a1f0634fa3003b9b0de00fb41cd1ca1f
Wanderson Alves <63158564+alves-wanderson@users.noreply.github.com>
2020-11-25 21:12:42 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const express = require("express");
+const spoilersRoute = require('./routes/spoilers');
+const sequelize = require('./database/database');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api', spoilersRoute);
+
+app.use((request, response, next) => {
+    response.status(404).send();
+});
+
+app.use((error, request, response, next) => {
+    response.status(500).json({ error });
+});
+
+sequelize.sync().then(() => {
+    const port = process.env.PORT || 3000;
+
+    app.set("port", port);
+
+    const server = http.createServer(app);
+
+    server.listen(port);
+});
